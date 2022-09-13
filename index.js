9f3a7a0f00e6ab4e0089586b3410c60ac16df56d
jamilagastar <jamilagastar@gmail.com>
2022-09-13 16:13:35 +1000

    amended project section, and about me

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', function() {
 // setInterval(function () {
 //     let name = document.querySelector('h1');
 //     name.style.visibility = (name.style.visibility == 'hidden' ? '' : 'hidden');
@@ -21,3 +21,15 @@ typingBrand(brand, 60).forEach(promise => {
     document.querySelector('p').innerHTML = portion;
   });
 });
+
+
+let imgElement = document.querySelector('figure#imagesSlideshow img')
+let images = ['./images/KanyeQuoteGenerator.png', './images/ReactProject.png'];
+setInterval(function(){
+    let random = Math.floor(Math.random() * 2);
+    imgElement.src = images[random];
+}, 700);
+
+}
+
+)
