003d8dd028cb98b62091f8e04006a2a41b9e3ad9
jamilagastar <jamilagastar@gmail.com>
2022-09-13 12:14:05 +1000

    amended CSS and JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
-setInterval(function () {
-    let name = document.querySelector('h1');
-    name.style.visibility = (name.style.visibility == 'hidden' ? '' : 'hidden');
-}, 900);
+// setInterval(function () {
+//     let name = document.querySelector('h1');
+//     name.style.visibility = (name.style.visibility == 'hidden' ? '' : 'hidden');
+// }, 900);
+
+const brand = 'JUNIOR SOFTWARE DEVELOPER & WEB DESIGNER.'
+
+const typingBrand = (brand, timeout) =>
+  [...brand].map(
+    (ch, i) =>
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve(brand.substring(0, i + 1));
+        }, timeout * i);
+      })
+  );
+
+typingBrand(brand, 60).forEach(promise => {
+  promise.then(portion => {
+    document.querySelector('p').innerHTML = portion;
+  });
+});
